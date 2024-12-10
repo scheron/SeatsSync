cf62b97c09143a3eab775f9b7948bcd4e63cd304
Oleg <infectedbyjs@gmail.com>
2024-12-11 02:19:42 +1000

    chore: Enhance WebSocket connection handling and simplify dotenv config usage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,16 +5,26 @@ import {WebSocketServer} from "ws"
 import {onMessage} from "./sockets/cinemas.socket"
 import {getAllCinemas} from "./models/cinemas.model"
 
-dotenv.config({path: "../../.env"})
+dotenv.config()
 
 const app = express()
 const server = http.createServer(app)
 const wss = new WebSocketServer({server})
 
+// TODO: Only single connection is allowed;
+// TODO: Need ping/pong to keep connection alive?
 wss.on("connection", (ws) => {
   console.log("connected")
 
   ws.on("message", (message) => {
+    // if(handlePingMessage(ws,message))return
+    if (message.toString() === "ping") {
+      console.log("ping")
+      ws.send("pong")
+    }
+
+    console.log("received: %s", message)
+
     onMessage(ws, message)
   })
 
