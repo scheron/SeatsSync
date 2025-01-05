dca0d88cdd579db7c9be708fbf6a6c32f71548bf
Oleg 👾 <infectedbyjs@gmail.com>
2025-01-05 17:58:18 +1000

    refactor: Update svg plugin

--- frontend/src/ui/common/base/BaseIcon.vue
+++ frontend/src/ui/common/base/BaseIcon.vue
@@ -4,15 +4,11 @@ import {cn} from "@/utils/tailwindcss"
 import type {IconName} from "@/types/icons"
 import type {ClassNameValue} from "tailwind-merge"
 
-const props = defineProps<{
-  name: IconName
-  size?: string
-  color?: string
-}>()
+defineProps<{name: IconName}>()
 </script>
 
 <template>
   <svg :class="cn('inline-block aspect-square size-6', $attrs.class as ClassNameValue)" fill="currentColor">
-    <use :href="`/icons-sprite.svg#${props.name}`" />
+    <use :href="`/icons-sprite.svg#${name}`" />
   </svg>
 </template>
