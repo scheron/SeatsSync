79e871b9507c266ca0b101b748947e2258cb2d37
Oleg 👾 <infectedbyjs@gmail.com>
2025-01-26 23:40:33 +1000

    fix: Adjust default error response status code to 200

--- backend/src/shared/messages/responses.ts
+++ backend/src/shared/messages/responses.ts
@@ -4,6 +4,6 @@ export function sendSuccess<T = any>(res: Response, data: T, statusCode: number 
   res.status(statusCode).json({data, status: "success", error: null})
 }
 
-export function sendError(res: Response, error: string, statusCode: number = 500): void {
+export function sendError(res: Response, error: string, statusCode: number = 200): void {
   res.status(statusCode).json({error, data: null, status: "error"})
 }
