ccced647aa674d787423d1c02c003e6393f5500c
Oleg 👾 <infectedbyjs@gmail.com>
2025-03-17 03:31:34 +1000

    refactor: Update SubscriptionOptions and related methods to use PartialDeep for improved type safety; remove unused BaseSelect and colorLog components

--- apps/backend/src/modules/cinema/cinema.methods.ts
+++ apps/backend/src/modules/cinema/cinema.methods.ts
@@ -2,10 +2,9 @@ import {formatSuccess} from "@/core/ws/messages"
 import * as CinemaService from "./cinema.service"
 
 import type {IWebSocketClient} from "@/core/ws"
-import type {Method} from "@seats-sync/constants/methods"
 import type {MessageRequest} from "@seats-sync/types/websocket"
 
-export async function getCinemas(ws: IWebSocketClient, message: MessageRequest<Method>) {
+export async function getCinemas(ws: IWebSocketClient, message: MessageRequest) {
   const cinemas = await CinemaService.getCinemas()
   ws.send(formatSuccess({eid: message.eid, type: message.type, status: "success", data: cinemas}))
 }
